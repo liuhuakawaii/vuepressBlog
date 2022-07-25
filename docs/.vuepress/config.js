dc099a2b0b3f47da5ff3d68e58114447c12a2a54
小董 <liuhuakawaii@163.com>
2022-07-25 10:27:51 +0800

    搭建完毕

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,13 @@
 module.exports = {
-  title: 'DL\'s blog',
+  title: '小董的Blog',
   description: '我的个人网站',
   head: [ // 注入到当前页面的 HTML <head> 中的标签
-    ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
-    ['link', { rel: 'manifest', href: '/photo.jpg' }],
-    ['link', { rel: 'apple-touch-icon', href: '/photo.jpg' }],  
+    ['link', { rel: 'icon', href: '/favicon.webp' }], // 增加一个自定义的 favicon(网页标签的图标)
+    // ['link', { rel: 'manifest', href: '/photo.jpg' }],
+    // ['link', { rel: 'apple-touch-icon', href: '/photo.jpg' }],  
+    // "script",{
+    //   src:"https://at.alicdn.com/t/font_2849934_v6y652peian.js"
+    // }
   ],
   
 serviceWorker: true, // 是否开启 PWA
@@ -12,19 +15,16 @@ serviceWorker: true, // 是否开启 PWA
   markdown: {
     lineNumbers: false // 代码块显示行号
   },
+  theme:'aurora',
   themeConfig:{
-    nav: [{text: "主页", link: "/"      },
-        { text: "node", link: "/node/" },
-        { text: "前端", 
-          items: [ 
-            { text: "html", link:"/web/html/"},
-            { text: "css", link:"/web/css/"},
-            ]
-        },
-        { text: "数据库", link: "/database/"   },
-        { text: "android", link: "/android/"   },
-        { text: "面试问题", link: "/interview/" }
-      ],
+    darkMode:false,
+    logo:'/avicon.webp',
+    nav:[
+      //导航栏配置
+      {text:'首页',link:'/'},
+      {text:'技术文档',link:'/tech/interview'},
+    ],
+    sidebar:'auto', //侧边栏配置
   }
 
 };
